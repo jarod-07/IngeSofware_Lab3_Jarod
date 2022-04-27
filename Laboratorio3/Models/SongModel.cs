604646e73353efa53a9bfaef6b481edcaf68eff4
JAROD DANIEL VENEGAS ALPIZAR <jarod.venegas@ucr.ac.cr>
2022-04-27 13:08:47 -0600

    Cambios en el view Song

--- Laboratorio3/Models/SongModel.cs
+++ Laboratorio3/Models/SongModel.cs
@@ -5,9 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Genre { get; set; }
-
         public string Artist { get; set; }
-
         public string Album { get; set; }
         public DateTime ReleasedDate { get; set; }
     }
